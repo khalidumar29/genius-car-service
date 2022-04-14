8a539d56ee6371630870ef17889ffe63f255d843
khalid umar <dreamiwoarld.online@gmail.com>
2022-04-14 14:29:39 +0600

    done carousel

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,14 +1,16 @@
-import React from "react";
-import Services from "../Services/Services";
-import Experts from "./Experts/Experts";
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Experts from '../Experts/Experts';
+import Services from '../Services/Services';
 
 const Home = () => {
-  return (
-    <div>
-      <Services></Services>
-      <Experts></Experts>
-    </div>
-  );
+    return (
+        <>
+            <Banner></Banner>
+            <Services></Services>
+            <Experts></Experts>
+        </>
+    );
 };
 
 export default Home;
