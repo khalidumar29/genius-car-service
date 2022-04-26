71c2a2ea5c5dbbde4db7385419a83a798ee3fea5
khalid umar <dreamiwoarld.online@gmail.com>
2022-04-26 22:16:20 +0600

    replace localhost:5000 url by the heroku server host

--- src/Hooks/useServiceDetail.js
+++ src/Hooks/useServiceDetail.js
@@ -1,17 +1,16 @@
 import { useEffect, useState } from "react";
 
-const useServiceDetail = serviceId =>{
-    const [service, setService] = useState({});
+const useServiceDetail = (serviceId) => {
+  const [service, setService] = useState({});
 
-    useEffect( () =>{
-        const url = `http://localhost:5000/service/${serviceId}`;
-        console.log(url);
-        fetch(url)
-        .then(res=> res.json())
-        .then(data => setService(data));
-
-    }, [serviceId]);
-    return [service]
-}
+  useEffect(() => {
+    const url = `https://genius-car-service11.herokuapp.com/service/${serviceId}`;
+    console.log(url);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, [serviceId]);
+  return [service];
+};
 
 export default useServiceDetail;
--- src/Hooks/useServiceDetails.js
+++ src/Hooks/useServiceDetails.js
@@ -4,7 +4,7 @@ const useServiceDetails = (serviceId) => {
   const [service, setService] = useState({});
 
   useEffect(() => {
-    fetch(`http://localhost:5000/service/${serviceId}`)
+    fetch(`https://genius-car-service11.herokuapp.com/service/${serviceId}`)
       .then((res) => res.json())
       .then((data) => setService(data));
   }, [serviceId]);
