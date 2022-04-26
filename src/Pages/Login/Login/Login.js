b86794bc45605022c53988d45709b6c9cf2fa49a
khalid umar <dreamiwoarld.online@gmail.com>
2022-04-26 06:18:55 +0600

    done jwt

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
 import {
@@ -29,19 +30,22 @@ const Login = () => {
   }
 
   if (user) {
-    navigate(from, { replace: true });
+    //  navigate(from, { replace: true });
   }
 
   if (error) {
     errorElement = <p className='text-danger'>Error: {error?.message}</p>;
   }
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
 
-    signInWithEmailAndPassword(email, password);
+    await signInWithEmailAndPassword(email, password);
+    const { data } = await axios.post("http://localhost:5000/login", { email });
+    localStorage.setItem("accessToken", data.accessToken);
+    navigate(from, { replace: true });
   };
 
   const navigateRegister = (event) => {
